31a9afbaf86240d8307a9d7b8c37cb99b83e3e56
Nagy Tibor <xnagytibor@protonmail.com>
2022-03-28 19:30:42 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,246 @@
+#![feature(io_read_to_string)]
+
+use std::error::Error;
+use std::path::PathBuf;
+
+use clap::Parser;
+use midly::num::{u15, u24, u28, u4, u7};
+use midly::MetaMessage;
+
+mod sv_model;
+use crate::sv_model::SvDocument;
+
+const MIDI_TICKS_PER_BEAT: usize = 1024;
+const MIDI_DRUM_CHANNEL: usize = 9;
+
+#[derive(Debug, Parser)]
+#[clap(author, version)]
+struct Args {
+    /// Input project file path
+    sv_input_path: PathBuf,
+
+    /// Converted MIDI file path
+    midi_output_path: PathBuf,
+
+    /// Fixed MIDI tempo used for exporting
+    #[clap(short = 't', long)]
+    tempo: Option<f64>,
+
+    /// Trim the leading silence before the first note
+    #[clap(short = 's', long)]
+    trim_leading_silence: bool,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+    let sv_document = SvDocument::load(&args.sv_input_path)?;
+
+    let sv_notes_layers = sv_document
+        .data
+        .layers
+        .iter()
+        .filter(|layer| layer.r#type == "notes")
+        .enumerate()
+        .map(|(channel_index, notes_layer)| {
+            // Skip drum channel
+            if channel_index < MIDI_DRUM_CHANNEL {
+                (channel_index, notes_layer)
+            } else {
+                (channel_index + 1, notes_layer)
+            }
+        })
+        .collect::<Vec<_>>();
+
+    let mut midi_document = midly::Smf::new(midly::Header::new(
+        midly::Format::SingleTrack,
+        midly::Timing::Metrical(u15::from(MIDI_TICKS_PER_BEAT as u16)),
+    ));
+
+    let midi_bpm = args.tempo.unwrap_or(120.0);
+    let mut midi_track = midly::Track::new();
+
+    // MIDI track initialization
+    {
+        midi_track.push(midly::TrackEvent {
+            delta: u28::from(0),
+            kind: midly::TrackEventKind::Meta(MetaMessage::Tempo(u24::from(
+                (60_000_000.0 / midi_bpm) as u32,
+            ))),
+        });
+
+        for &(channel_index, notes_layer) in sv_notes_layers.iter() {
+            midi_track.push(midly::TrackEvent {
+                delta: u28::from(0),
+                kind: midly::TrackEventKind::Meta(midly::MetaMessage::MidiChannel(u4::from(
+                    channel_index as u8,
+                ))),
+            });
+
+            midi_track.push(midly::TrackEvent {
+                delta: u28::from(0),
+                kind: midly::TrackEventKind::Meta(midly::MetaMessage::InstrumentName(
+                    notes_layer.name.as_bytes(),
+                )),
+            });
+
+            let play_parameters = sv_document
+                .get_play_parameters_by_id(notes_layer.model)
+                .expect("failed to find play parameters");
+
+            midi_track.push(midly::TrackEvent {
+                delta: u28::from(0),
+                kind: midly::TrackEventKind::Midi {
+                    channel: u4::from(channel_index as u8),
+                    message: midly::MidiMessage::ProgramChange {
+                        program: play_parameters.midi_program(),
+                    },
+                },
+            });
+
+            if play_parameters.mute {
+                midi_track.push(midly::TrackEvent {
+                    delta: u28::from(0),
+                    kind: midly::TrackEventKind::Midi {
+                        channel: u4::from(channel_index as u8),
+                        message: midly::MidiMessage::Controller {
+                            controller: u7::from(7),
+                            value: u7::from(0),
+                        },
+                    },
+                });
+            } else {
+                // TODO: play_parameters.gain
+                // Input range: 0.0-4.0, default 1.0
+                // MIDI range: 0-127, default 100
+            }
+
+            midi_track.push(midly::TrackEvent {
+                delta: u28::from(0),
+                kind: midly::TrackEventKind::Midi {
+                    channel: u4::from(channel_index as u8),
+                    message: midly::MidiMessage::Controller {
+                        controller: u7::from(10),
+                        value: u7::from((64.0 + (play_parameters.pan * 63.5)) as u8),
+                    },
+                },
+            });
+        }
+    }
+
+    // Emitting MIDI track data
+    {
+        struct AbsoluteMidiEvent {
+            channel_index: usize,
+            key: usize,
+            ticks: usize,
+            note_on: bool,
+        }
+
+        let mut absolute_midi_events = sv_notes_layers
+            .iter()
+            .flat_map(|&(channel_index, notes_layer)| {
+                let model = sv_document
+                    .get_model_by_id(notes_layer.model)
+                    .expect("notes layer doesn't have model specified");
+
+                let dataset_id = model.dataset.expect("model doesn't have dataset specified");
+                let dataset = sv_document
+                    .get_dataset_by_id(dataset_id)
+                    .expect("dataset doesn't exist");
+
+                dataset.points.iter().flat_map(move |point| {
+                    let key = point
+                        .value
+                        .expect("notes layer point has no value specified");
+
+                    let duration = point
+                        .duration
+                        .expect("notes layer point has no duration specified");
+
+                    let offset_seconds = (point.frame as f64) / (model.sample_rate as f64);
+                    let length_seconds = (duration as f64) / (model.sample_rate as f64);
+
+                    let seconds_to_ticks = |seconds: f64| -> usize {
+                        (seconds * (midi_bpm / 60.0) * MIDI_TICKS_PER_BEAT as f64) as usize
+                    };
+
+                    [
+                        // Note on event
+                        AbsoluteMidiEvent {
+                            channel_index,
+                            key,
+                            ticks: seconds_to_ticks(offset_seconds),
+                            note_on: true,
+                        },
+                        // Note off event
+                        AbsoluteMidiEvent {
+                            channel_index,
+                            key,
+                            ticks: seconds_to_ticks(offset_seconds + length_seconds),
+                            note_on: false,
+                        },
+                    ]
+                })
+            })
+            .collect::<Vec<_>>();
+
+        absolute_midi_events.sort_by_key(
+            |&AbsoluteMidiEvent {
+                 channel_index,
+                 key,
+                 ticks,
+                 note_on,
+             }| {
+                // Rationale behind this sorting key:
+                // - ticks: Interleave the channels and sort events by occurrence.
+                // - note_on: When multiple events occur at the same time, emit the
+                //     note off events first before any new note on events.
+                // - channel_index: Make sorting results reproducible.
+                // - key: Make sorting results reproducible.
+                (ticks, note_on, channel_index, key)
+            },
+        );
+
+        for (event_index, event) in absolute_midi_events.iter().enumerate() {
+            let delta_time = if event_index == 0 {
+                if args.trim_leading_silence {
+                    0
+                } else {
+                    event.ticks
+                }
+            } else {
+                absolute_midi_events[event_index].ticks
+                    - absolute_midi_events[event_index - 1].ticks
+            };
+
+            if event.note_on {
+                midi_track.push(midly::TrackEvent {
+                    delta: u28::from(delta_time as u32),
+                    kind: midly::TrackEventKind::Midi {
+                        channel: u4::from(event.channel_index as u8),
+                        message: midly::MidiMessage::NoteOn {
+                            key: u7::from(event.key as u8),
+                            vel: u7::from(64 as u8),
+                        },
+                    },
+                });
+            } else {
+                midi_track.push(midly::TrackEvent {
+                    delta: u28::from(delta_time as u32),
+                    kind: midly::TrackEventKind::Midi {
+                        channel: u4::from(event.channel_index as u8),
+                        message: midly::MidiMessage::NoteOff {
+                            key: u7::from(event.key as u8),
+                            vel: u7::from(0 as u8),
+                        },
+                    },
+                });
+            }
+        }
+    }
+
+    midi_document.tracks.push(midi_track);
+    midi_document.save(args.midi_output_path)?;
+
+    Ok(())
+}
---
+++ src/sv_model.rs
@@ -0,0 +1,239 @@
+use std::error::Error;
+use std::fs::File;
+use std::io;
+use std::path::Path;
+
+use bzip2_rs::DecoderReader;
+use midly::num::u7;
+use strong_xml::XmlRead;
+
+#[derive(Debug, XmlRead)]
+#[xml(tag = "sv")]
+pub struct SvDocument {
+    #[xml(child = "data")]
+    pub data: SvData,
+
+    #[xml(child = "display")]
+    pub display: SvDisplay,
+
+    #[xml(child = "selections")]
+    pub selections: SvSelections,
+}
+
+#[derive(Debug, XmlRead)]
+#[xml(tag = "data")]
+pub struct SvData {
+    #[xml(child = "model")]
+    pub models: Vec<SvModel>,
+
+    #[xml(child = "playparameters")]
+    pub play_parameters: Vec<SvPlayParameters>,
+
+    #[xml(child = "layer")]
+    pub layers: Vec<SvLayer>,
+
+    #[xml(child = "dataset")]
+    pub datasets: Vec<SvDataset>,
+}
+
+#[derive(Debug, XmlRead)]
+#[xml(tag = "model")]
+pub struct SvModel {
+    #[xml(attr = "id")]
+    pub id: usize,
+
+    #[xml(attr = "name")]
+    pub name: String,
+
+    #[xml(attr = "sampleRate")]
+    pub sample_rate: usize,
+
+    #[xml(attr = "start")]
+    pub start: usize,
+
+    #[xml(attr = "end")]
+    pub end: usize,
+
+    #[xml(attr = "type")]
+    pub r#type: String,
+
+    #[xml(attr = "file")]
+    pub file: Option<String>,
+
+    #[xml(attr = "mainModel")]
+    pub main_model: Option<bool>,
+
+    #[xml(attr = "dimensions")]
+    pub dimensions: Option<usize>,
+
+    #[xml(attr = "resolution")]
+    pub resolution: Option<usize>,
+
+    #[xml(attr = "notifyOnAdd")]
+    pub notify_on_add: Option<bool>,
+
+    #[xml(attr = "dataset")]
+    pub dataset: Option<usize>,
+
+    #[xml(attr = "subtype")]
+    pub subtype: Option<String>,
+
+    #[xml(attr = "valueQuantization")]
+    pub value_quantization: Option<usize>,
+
+    #[xml(attr = "minimum")]
+    pub minimum: Option<usize>,
+
+    #[xml(attr = "maximum")]
+    pub maximum: Option<usize>,
+
+    #[xml(attr = "units")]
+    pub units: Option<String>,
+}
+
+#[derive(Debug, XmlRead)]
+#[xml(tag = "playparameters")]
+pub struct SvPlayParameters {
+    #[xml(attr = "mute")]
+    pub mute: bool,
+
+    #[xml(attr = "pan")]
+    pub pan: f64,
+
+    #[xml(attr = "gain")]
+    pub gain: f64,
+
+    #[xml(attr = "clipId")]
+    pub clip_id: String,
+
+    #[xml(attr = "model")]
+    pub model: usize,
+
+    #[xml(child = "plugin")]
+    pub plugins: Vec<SvPlugin>,
+}
+
+#[derive(Debug, XmlRead)]
+#[xml(tag = "plugin")]
+pub struct SvPlugin {
+    #[xml(attr = "identifier")]
+    pub identifier: String,
+
+    #[xml(attr = "program")]
+    pub program: String,
+}
+
+#[derive(Debug, XmlRead)]
+#[xml(tag = "dataset")]
+pub struct SvDataset {
+    #[xml(attr = "id")]
+    pub id: usize,
+
+    #[xml(attr = "dimensions")]
+    pub dimensions: usize,
+
+    #[xml(child = "point")]
+    pub points: Vec<SvPoint>,
+}
+
+#[derive(Debug, XmlRead)]
+#[xml(tag = "point")]
+pub struct SvPoint {
+    #[xml(attr = "frame")]
+    pub frame: usize,
+
+    #[xml(attr = "value")]
+    pub value: Option<usize>,
+
+    #[xml(attr = "duration")]
+    pub duration: Option<usize>,
+
+    #[xml(attr = "level")]
+    pub level: Option<f64>,
+
+    #[xml(attr = "label")]
+    pub label: String,
+}
+
+#[derive(Debug, XmlRead)]
+#[xml(tag = "layer")]
+pub struct SvLayer {
+    #[xml(attr = "id")]
+    pub id: usize,
+
+    #[xml(attr = "type")]
+    pub r#type: String,
+
+    #[xml(attr = "name")]
+    pub name: String,
+
+    #[xml(attr = "model")]
+    pub model: usize,
+    // TODO: Other properties
+}
+
+#[derive(Debug, XmlRead)]
+#[xml(tag = "display")]
+pub struct SvDisplay {
+    // stub
+}
+
+#[derive(Debug, XmlRead)]
+#[xml(tag = "selections")]
+pub struct SvSelections {
+    // stub
+}
+
+impl SvDocument {
+    pub fn load(path: &Path) -> Result<Self, Box<dyn Error>> {
+        let mut bzip2_decoder = DecoderReader::new(File::open(path)?);
+        let xml_data = io::read_to_string(&mut bzip2_decoder)?;
+
+        Ok(SvDocument::from_str(&xml_data)?)
+    }
+
+    pub fn get_layer_by_id(&self, id: usize) -> Option<&SvLayer> {
+        self.data.layers.iter().find(|layer| layer.id == id)
+    }
+
+    pub fn get_model_by_id(&self, id: usize) -> Option<&SvModel> {
+        self.data.models.iter().find(|model| model.id == id)
+    }
+
+    pub fn get_dataset_by_id(&self, id: usize) -> Option<&SvDataset> {
+        self.data.datasets.iter().find(|dataset| dataset.id == id)
+    }
+
+    pub fn get_play_parameters_by_id(&self, id: usize) -> Option<&SvPlayParameters> {
+        self.data
+            .play_parameters
+            .iter()
+            .find(|play_parameters| play_parameters.model == id)
+    }
+}
+
+impl SvPlayParameters {
+    pub fn midi_program(&self) -> u7 {
+        u7::from(match self.clip_id.as_str() {
+            "piano" => 0,
+            "elecpiano" => 5,
+            "organ" => 17,
+            "bass" => 32,
+            "beep" => 80,
+            // Percussion sounds mapped to some random MIDI instruments until
+            // MIDI drum channel support gets implemented.
+            "bounce" => 127,
+            "clap" => 118,
+            "click" => 117,
+            "cowbell" => 112,
+            "hihat" => 117,
+            "kick" => 117,
+            "silent" => 0,
+            "snare" => 117,
+            "stick" => 115,
+            "strike" => 127,
+            "tap" => 127,
+            _ => 0,
+        })
+    }
+}
